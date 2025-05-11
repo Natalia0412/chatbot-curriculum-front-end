5ae441be2ae655a9de4975a3c4e5415aa1019d77
Natalia Barros <natalia.vieiramonteiro70@gmail.com>
2025-05-10 22:45:45 -0300

    criei o upload

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ChatComponent } from './pages/chat/chat.component';
+import { HttpClientModule } from '@angular/common/http';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChatComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    HttpClientModule,
+    MatSnackBarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/pages/chat/chat.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit } from '@angular/core';
+import { ChatbotService } from '../../services/chatbot.service';
+import { MatSnackBar, MatSnackBarDismiss } from '@angular/material/snack-bar';
+
+
+@Component({
+  selector: 'app-chat',
+  templateUrl: './chat.component.html',
+  styleUrls: ['./chat.component.scss']
+})
+export class ChatComponent implements OnInit {
+
+  pdfFile: File | null = null;
+  chatbotResponse = '';
+  question = '';
+  filter = '';
+  historical: any[] = [];
+  responseData: any;
+
+  constructor(private chatbotService: ChatbotService, private snackBar: MatSnackBar) { }
+
+  ngOnInit(): void {
+  }
+
+  onFileSelected(event: any) {
+    this.pdfFile = event.target.files[0];
+  }
+
+  upload(){
+    if(this.pdfFile){
+      const formData = new FormData();
+      formData.append('pdf_file', this.pdfFile)
+      this.chatbotService.uploadPDF(formData).subscribe((res) => {
+        this.responseData = res.response;
+        this.snackBar.open(res.response.mensagem || 'Currículo enviado!', 'Fechar', {
+          duration: 3000
+        });
+      })
+    }
+  }
+
+
+}
