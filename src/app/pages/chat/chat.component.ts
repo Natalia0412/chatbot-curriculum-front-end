c3cae6dc779a0f0e3623ae87d5a7838311d2aedf
Natalia Barros <natalia.vieiramonteiro70@gmail.com>
2025-05-12 23:22:01 -0300

    Botao carregar que eh referente ao carregar dados esta funcioando

--- src/app/pages/chat/chat.component.ts
+++ src/app/pages/chat/chat.component.ts
@@ -17,6 +17,8 @@ export class ChatComponent implements OnInit {
   historical: any[] = [];
   responseData: any;
   selectedFile = false;
+  control_button_load_faiss: boolean = false;
+  control_submit_button_upload: boolean = false;
 
   constructor(private chatbotService: ChatbotService, private snackBar: MatSnackBar) { }
 
@@ -27,18 +29,49 @@ export class ChatComponent implements OnInit {
     this.pdfFile = event.target.files[0];
   }
 
-  upload(){
-    if(this.pdfFile){
+  upload() {
+    if (this.pdfFile) {
+
       const formData = new FormData();
       formData.append('pdf_file', this.pdfFile)
-      this.chatbotService.uploadPDF(formData).subscribe((res) => {
-        this.responseData = res.response;
-        this.snackBar.open(res.response.mensagem || 'Currículo enviado!', 'Fechar', {
-          duration: 3000
-        });
+
+      this.chatbotService.uploadPDF(formData).subscribe({
+        next: (res) => {
+          this.responseData = res.response;
+          this.snackBar.open(res.response.mensagem || 'Currículo enviado!', 'Fechar', {
+            duration: 7000,
+          });
+          this.control_submit_button_upload = true
+          //  setTimeout(() => {
+          //   this.control_submit_button_upload = false;
+          // }, 9000);
+
+        },
+        error: (err) => {
+          this.snackBar.open(err.error.mensagem || 'Erro ao enviar currículo!', 'Fechar', {
+            duration: 7000
+          });
+          this.control_submit_button_upload = false
+        }
       })
     }
   }
 
+  loadData() {
+    if (this.responseData) {
+      this.chatbotService.loadData(this.responseData).subscribe({
+        next: (res) => {
+          this.snackBar.open(res.response.mensagem || 'Dados carregados com sucesso!', 'Fechar', { duration: 7000 });
+          this.control_button_load_faiss = true;
+        },
+        error: (err) => {
+          this.snackBar.open(err.error.mensagem || 'Erro ao carregar dados!', 'Fechar', {
+            duration: 7000
+          });
+          this.control_button_load_faiss = false;
+        }
+      });
+    }
+  }
 
 }
